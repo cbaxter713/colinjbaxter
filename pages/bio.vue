06b95e4d7adca3dd46f161cf94271cf9f707c38d
Colin Baxter <colin.baxter@zaneray.com>
2018-01-08 22:50:35 -0700

    Adding a basic bio page with nuxt-link for routing

---
+++ pages/bio.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="container">
+    <h1 class="title">This is the Bio Page</h1>
+    <div class="links">
+      <nuxt-link to="/" class="button--green">Home</nuxt-link>
+    </div>
+  </section>
+</template>
+
+<script>
+
+</script>
+
+<style>
+  .container {
+    display: block;
+    max-width: 1300px;
+    margin: 0 auto;
+    padding: 0 20px;
+  }
+
+  .title {
+    font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
+
+  .links {
+    padding-top: 15px;
+  }
+</style>
